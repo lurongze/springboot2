063cab2036a173a0c4a0a2d13341f05e3ceb6c07
lurongze <1946755280@qq.com>
2018-06-04 18:13:17 +0800

    1

---
+++ src/main/java/com/lrz/service/UserService.java
@@ -0,0 +1,11 @@
+package com.lrz.service;
+import com.lrz.model.User;
+import com.lrz.core.Service;
+
+
+/**
+ * Created by CodeGenerator on 2018/06/04.
+ */
+public interface UserService extends Service<User> {
+
+}

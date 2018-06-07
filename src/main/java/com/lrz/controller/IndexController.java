385d3354c8e36b520597aa0569b0ae2efaffa2b2
lurongze <1946755280@qq.com>
2018-06-07 10:10:10 +0800

    集成redis操作

--- src/main/java/com/lrz/controller/IndexController.java
+++ src/main/java/com/lrz/controller/IndexController.java
@@ -1,9 +1,14 @@
 package com.lrz.controller;
 
+import com.lrz.core.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Date;
 
 /**
  * Created by gz000172 on 2018/5/14.
@@ -11,8 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class IndexController extends BaseController{
+    @Resource
+    private RedisService redisService;
     @GetMapping("/index")
     public String index() {
+        redisService.setStr("testKey", "list-action" + new Date().toString());
         return "list-action";
     }
 
---
+++ src/main/java/com/lrz/core/RedisService.java
@@ -0,0 +1,81 @@
+package com.lrz.core;
+
+/**
+ * Created by gz000172 on 2018/6/7.
+ */
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+/**
+ * Created by on 2017/3/1.
+ */
+@Service
+public class RedisService {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Resource(name = "stringRedisTemplate")
+    private ValueOperations<String, String> valOpsStr;
+
+    @Resource
+    RedisTemplate<Object, Object> redisTemplate;
+
+    @Resource(name = "redisTemplate")
+    private ValueOperations<Object, Object> valOpsObj;
+
+    /**
+     * 根据指定key获取String
+     * @param key
+     * @return
+     */
+    public String getStr(String key){
+        return valOpsStr.get(key);
+    }
+
+    /**
+     * 设置Str缓存
+     * @param key
+     * @param val
+     */
+    public void setStr(String key, String val){
+        valOpsStr.set(key,val);
+    }
+
+    /**
+     * 删除指定key
+     * @param key
+     */
+    public void del(String key){
+        stringRedisTemplate.delete(key);
+    }
+
+    /**
+     * 根据指定o获取Object
+     * @param o
+     * @return
+     */
+    public Object getObj(Object o){
+        return valOpsObj.get(o);
+    }
+
+    /**
+     * 设置obj缓存
+     * @param o1
+     * @param o2
+     */
+    public void setObj(Object o1, Object o2){
+        valOpsObj.set(o1, o2);
+    }
+
+    /**
+     * 删除Obj缓存
+     * @param o
+     */
+    public void delObj(Object o){
+        redisTemplate.delete(o);
+    }
+
+}

054e712a49ea8095c6f3a973337856ecfe849abe
lurongze <1946755280@qq.com>
2018-08-03 18:02:06 +0800

    1

--- src/test/resources/sql/springboot.sql
+++ src/test/resources/sql/springboot.sql
@@ -2,7 +2,7 @@
 --
 -- Host: 127.0.0.1    Database: springboot
 -- ------------------------------------------------------
--- Server version	5.5.53
+-- Server version	8.0.11
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -39,8 +39,36 @@ CREATE TABLE `open_user` (
 
 LOCK TABLES `open_user` WRITE;
 /*!40000 ALTER TABLE `open_user` DISABLE KEYS */;
-INSERT INTO `open_user` VALUES ('wxe22a8310f6aadeff','oP_cK0TkMzc86uqhgBmWUq_OdPs8','81C3DAA1AC506AC8FA492BD4F4CD73B4','azTj8aNggdEAMBXVB/6Zcg==',NULL,'');
+INSERT INTO `open_user` VALUES ('wxe22a8310f6aadeff','oP_cK0TkMzc86uqhgBmWUq_OdPs8','27AB627DE9846E8AB0B70829D316BED8','aQttT4v2f0vByyxqOpLu0Q==',NULL,'');
 /*!40000 ALTER TABLE `open_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_type`
+--
+
+DROP TABLE IF EXISTS `product_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `union_id` varchar(50) NOT NULL,
+  `cate_name` varchar(100) DEFAULT NULL COMMENT '分类名称',
+  `cate_logo` varchar(200) DEFAULT NULL COMMENT '分类图标',
+  `is_show` tinyint(4) DEFAULT '1',
+  `is_delete` tinyint(4) DEFAULT '0',
+  `sort_order` int(11) DEFAULT '50',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='产品分类表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_type`
+--
+
+LOCK TABLES `product_type` WRITE;
+/*!40000 ALTER TABLE `product_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -103,7 +131,7 @@ CREATE TABLE `union_group` (
 
 LOCK TABLES `union_group` WRITE;
 /*!40000 ALTER TABLE `union_group` DISABLE KEYS */;
-INSERT INTO `union_group` VALUES (1,'2222222222','','sjfieshefsjfkfjslkdsjflkdfj',NULL,0),(2,'12312',NULL,'31231',NULL,0),(3,'123123','b5b40a7a-d65e-47e3-91ca-787064796b67','1231232',NULL,0);
+INSERT INTO `union_group` VALUES (1,'橙蓝','18a411e4-30d1-40cf-ad25-5a579cbd5edf','这是第一个组织，你好',NULL,0),(2,'小程序柠檬商城','3ea86196-7507-4052-a4d4-4bb9487cba85','小程序柠檬商城1',NULL,0),(3,'1231239999','b5b40a7a-d65e-47e3-91ca-787064796b67','44444444444',NULL,1);
 /*!40000 ALTER TABLE `union_group` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -125,7 +153,7 @@ CREATE TABLE `user` (
   `union_id` varchar(50) NOT NULL COMMENT '组织id',
   `is_delete` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -134,33 +162,8 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'lurongze','','','F730FD868D1550C7B41D783DD67FE6FF',1529748103,'eyJsb2dpblRpbWUiOjE1MzIzMzcyMzMsIm5hbWUiOiJsdXJvbmd6ZSIsInNpZ24iOiJBQUVFMjE1MEFFRkFDMzlENTVENzQ3MEExOENGQzc4QyIsImlkIjoxfQ==','',0),(2,'陆荣泽4','1946755280@qq.com','广州市','21312312',NULL,'','0',0),(3,'AAAA','AAAA','AAQWEQ','41D58883E50B53870FCA159FCEF8A1F8',1531195985,'','0',1),(4,'小明','123123','123123','67BDE276A6182E41FDF7DC7F629A1114',1531210264,'','0',1),(5,'小红','123123','12312','5C7384FD5D8572DA2EDA746E7F2B51E4',1531213557,'','0',1),(6,'22222','111','1111','FC9D41C66E354D20D799ECB0F60C2C64',1531217321,'','0',1);
+INSERT INTO `user` VALUES (1,'lurongze','','','F730FD868D1550C7B41D783DD67FE6FF',1529748103,'eyJsb2dpblRpbWUiOjE1MzMyODQ2OTYsIm5hbWUiOiJsdXJvbmd6ZSIsInNpZ24iOiIxQkNGRjhDNEQzQjE4RTRGM0E5NTIzQzM5NjhCMDdBQSIsImlkIjoxfQ==','',0),(2,'陆荣泽4','1946755280@qq.com','广州市','21312312',NULL,'','0',1),(3,'AAAA','AAAA','AAQWEQ','41D58883E50B53870FCA159FCEF8A1F8',1531195985,'','0',1),(4,'小明','123123','123123','67BDE276A6182E41FDF7DC7F629A1114',1531210264,'','0',1),(5,'小红','123123','12312','5C7384FD5D8572DA2EDA746E7F2B51E4',1531213557,'','0',1),(6,'22222','111','1111','FC9D41C66E354D20D799ECB0F60C2C64',1531217321,'','0',1),(7,'1231231','123123','12312313','1C1A76F3B72AB40FAB215B623CAB21F6',1532657165,'','0',1),(8,'lrz','123123','123131','F730FD868D1550C7B41D783DD67FE6FF',1532657262,'eyJsb2dpblRpbWUiOjE1MzI2ODQ1NDgsIm5hbWUiOiJscnoiLCJzaWduIjoiODYyMkZFMUM1Nzc1NUM5Q0JDNkNGRTNEMjA1RTEwN0YiLCJpZCI6OH0=','18a411e4-30d1-40cf-ad25-5a579cbd5edf',0),(9,'12312312313','12313','12313','A29FB138C6EBB76D7D94D9E9F5800389',1532657267,'','0',1),(10,'12312312','312312312312','3123123123','5219A99933DD63BB2A5AC74214E462F3',1532657317,'','0',1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user_info`
---
-
-DROP TABLE IF EXISTS `user_info`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_info` (
-  `id` int(9) NOT NULL AUTO_INCREMENT,
-  `uid` int(9) NOT NULL,
-  `phone` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_info`
---
-
-LOCK TABLES `user_info` WRITE;
-/*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
-INSERT INTO `user_info` VALUES (1,41,'15021618114'),(2,1,'15021618114'),(3,1,'15021618114'),(4,1,'15021618114'),(5,1,'15021618114');
-/*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -172,10 +175,12 @@ DROP TABLE IF EXISTS `user_role`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_role` (
   `id` int(9) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `role` varchar(20) NOT NULL,
+  `union_id` varchar(20) NOT NULL,
+  `role_name` varchar(255) NOT NULL,
+  `is_delete` tinyint(4) DEFAULT '0',
+  `sort_order` int(11) DEFAULT '50',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -184,6 +189,7 @@ CREATE TABLE `user_role` (
 
 LOCK TABLES `user_role` WRITE;
 /*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+INSERT INTO `user_role` VALUES (1,'0','管理员',0,50),(2,'0','产品管理员',0,51),(3,'0','66666666',1,0);
 /*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -196,4 +202,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-23 17:14:30
+-- Dump completed on 2018-08-03 18:01:48
